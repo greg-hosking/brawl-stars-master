da38af1bd5e17a998a682f1a26338f83e1b90444
Greg Hosking <hoskinggregory@gmail.com>
2022-08-13 15:00:01 -0400

    Add Prettier to app and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,38 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import { Image, Nav } from 'react-bootstrap';
+import Navigation from './components/shared/Navigation';
+import Footer from './components/shared/Footer';
 
 import HomePage from './pages/HomePage';
 import BrawlersPage from './pages/BrawlersPage';
+import BrawlerPage from './pages/BrawlerPage';
 import EventsPage from './pages/EventsPage';
 import GameModesPage from './pages/GameModesPage';
+import { Container } from 'react-bootstrap';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  NavLink
-} from 'react-router-dom';
-
-
-function NavigationBar() {
+const App: React.FunctionComponent = () => {
   return (
-    <div className='navigation-bar'>
-      <Navbar variant='dark' bg='transparent' expand='sm' sticky='top'>
-        <Container>
-          <Navbar.Brand as={NavLink} to="/">
-            <Image src='https://cdn.brawlify.com/gamemode/Hunters.png' alt="Brawl Stars Companion logo" height={60}></Image>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar'></Navbar.Toggle>
-          <Navbar.Collapse id='navbar'>
-            <Nav fill style={{ width: '100%' }}>
-              <Nav.Link as={NavLink} to="/" className='d-sm-none d-md-block'><h3>HOME</h3></Nav.Link>
-              <Nav.Link as={NavLink} to="/brawlers"><h3>BRAWLERS</h3></Nav.Link>
-              <Nav.Link as={NavLink} to="/events"><h3>EVENTS</h3></Nav.Link>
-              <Nav.Link as={NavLink} to="/gamemodes"><h3>GAME MODES</h3></Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  )
-}
-
-function Footer() {
-  return (
-    <div className='footer px-5 py-3'>
-      <h5>
-        Powered by <a href='https://brawlify.com/' target='_blank' rel='noopener noreferrer'>Brawlify</a>.
-      </h5>
-      <h5>
-        This material is unofficial and is not endorsed by Supercell.
-        For more information see <a href='https://supercell.com/en/fan-content-policy/' target='_blank' rel='noopener noreferrer'>
-          Supercell's Fan Content Policy.</a>
-      </h5>
-    </div>
-  )
-}
-
-function App() {
-  return (
-    <>
+    <Container fluid className='app-container p-0'>
       <BrowserRouter>
         <header>
-          <NavigationBar />
+          <Navigation />
         </header>
         <main>
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/brawlers' element={<BrawlersPage />} />
+            <Route path='/brawlers/:id' element={<BrawlerPage />} />
             <Route path='/events' element={<EventsPage />} />
             <Route path='/gamemodes' element={<GameModesPage />} />
           </Routes>
         </main>
         <Footer />
       </BrowserRouter>
-    </>
+    </Container>
   );
-}
+};
 
 export default App;
